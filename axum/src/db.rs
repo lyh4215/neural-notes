a247f502e389b4139800d290a195ebb3a706b6c8
lyh4215 <lyh4215@kaist.ac.kr>
2025-07-03 10:56:52 +0900

    perf : set max connection in DB

--- axum/src/db.rs
+++ axum/src/db.rs
@@ -1,15 +1,22 @@
 // src/db.rs
 
 use sqlx::{SqlitePool, sqlite::SqliteConnectOptions};
+
 pub async fn init_db() -> SqlitePool {
     let db_path = std::env::var("DATABASE_PATH").unwrap_or_else(|_| "./posts.db".to_string());
     let options = SqliteConnectOptions::new()
         .filename(db_path)
         .create_if_missing(true);
 
-    let db = SqlitePool::connect_with(options)
+    let db = sqlx::sqlite::SqlitePoolOptions::new()
+        .max_connections(5) // Set the maximum number of connections
+        .connect_with(options)
         .await
         .expect("DB connection failed");
-    sqlx::migrate!().run(&db).await.unwrap();
+
+    sqlx::migrate!()
+        .run(&db)
+        .await
+        .expect("Database migration failed");
     db
 }
